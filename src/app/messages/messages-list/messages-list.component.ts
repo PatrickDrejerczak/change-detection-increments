fd14b26c17214f06d028a2ad4d14352e5393e037
PatrickDrejerczak <drejerczak@aol.com>
2024-07-29 22:30:24 +0200

    added OnPush detection and messages service

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -1,16 +1,24 @@
-import { Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+} from '@angular/core';
+import { MessagesService } from '../../services/messages.service';
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MessagesListComponent {
-  messages = input.required<string[]>();
-
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
     return 'MessagesList Component Debug Output';
   }
+
+  private messagesService = inject(MessagesService);
+  messages = this.messagesService.allMessages;
 }
