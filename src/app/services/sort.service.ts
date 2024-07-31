2b40eea150b704740b19122f710c2de84d77282e
PatrickDrejerczak <drejerczak@aol.com>
2024-07-31 21:48:56 +0200

    removed unnecessary service

--- src/app/services/sort.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({ providedIn: 'root' })
-export class SortService {
-  sort<T>(array: T[], compareFn: (a: T, b: T) => number): T[] {
-    return array.slice().sort(compareFn);
-  }
-}
